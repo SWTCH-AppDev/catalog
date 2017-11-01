ce84c0588edbc097e1c29bcf696f68a88a4a62dd
Carter Li <carter@swtchev.com>
2017-11-01 18:50:33 +0000

    Google maps fix

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,6 +7,20 @@ class FoodsController < ApplicationController
 
   def show
     @food = Food.find(params[:id])
+    
+    @street_address = @food.store_address.gsub(" ", "+")
+
+    require 'open-uri'
+    
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#@street_address}&"
+  
+    parsed_data = JSON.parse(open(url).read)
+
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    
+    @address_title = parsed_data["results"][0]["formatted_address"]
 
     render("foods_templates/show.html.erb")
   end
